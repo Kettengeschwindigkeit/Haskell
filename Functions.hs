33e8d950006590c765237901e1bf71873729623f
Kettengeschwindigkeit <666damned999@mail.ru>
2024-08-31 11:21:12 +0300

    #10 Functions

--- Functions.hs
+++ Functions.hs
@@ -3,8 +3,8 @@ module Functions where
   getArccos = acos (cos pi)
 
   getMax = max 5 42
-  -- аналогично (операция применения функции ассоциативна влево)
-  getMax1 = (max 5) 42
+  
+  getMax1 = (max 5) 42                            -- аналогично (операция применения функции ассоциативна влево)
   
   function1 = 3 + sin 42
   function2 = 3 + (max 5) 42
@@ -16,3 +16,9 @@ module Functions where
   -- условные выражения
   function3 x = if x > 0 then 1 else (-1)
   function4 x = (if x > 0 then 1 else (-1)) + 3
+
+  max5 x = max 5 x
+  max5' = max 5                                   -- аналогично
+
+  discount limit perc sum = if sum >= limit then sum * (100 - perc) / 100 else sum
+  standardDiscount = discount 1000 5
